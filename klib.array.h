dd5c2026989c032561f41ef1207064f989a95116
Apisit Ritreungroj <apisit_lon@hotmail.com>
2020-03-15 20:14:32 +0700

    Update klib.array.h

--- klib.array.h
+++ klib.array.h
@@ -1,10 +1,33 @@
-#ifndef KLIB_STRING_H
-#define KLIB_STRING_H
+#ifndef KLIB_ARRAY_H
+#define KLIB_ARRAY_H
 
-template <class data_type>
-typedef class Array {
+template<class _type>
+class Array {
     private:
-
+        _type *data;
     public:
+        unsigned length;
+    
+        Array(const std::initializer_list<_type> list);
+        
+        _type push();
+        _type pop();
+        _type shift();
+        _type unshift();
+};
 
-} array
+template<class _type>
+Array<_type>::Array(const std::initializer_list<_type> list) {
+    length = list.size(); //list.end()-list.begin()
+    
+    data = new _type[length];
+    
+    for (unsigned i = 0; i < length; i++) {
+        std::cout << *(list.begin()+i) << "\n";
+    }
+}
+
+template<class _type>
+using array = Array<_type>;
+
+#endif
